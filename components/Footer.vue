fb9d08114154c832558ae89a9ea9e9cdc9edc5dd
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-03-23 21:50:01 +0100

    Started on footer

---
+++ components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+  <footer class="section section--none-mb section--none-pb section--none-pt">
+    <div class="one-whole footer-container">
+      <div class="one-whole pt-40">
+        <div class="two-thirds">
+          <p>© Marius Pedersen 2022</p>
+        </div>
+        <div class="one-third space-evenly">
+          <a
+            class=""
+            href="https://www.linkedin.com/in/marius-pedersen-997024156/"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Linkedin</a
+          >
+          <a
+            class=""
+            href="https://github.com/MariusPedersen95"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Github</a
+          >
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.logo-symbol-container {
+  width: 100px;
+}
+
+.footer-container {
+  border-top: 3px solid var(--c-white);
+}
+.margin-tp {
+  margin-top: 1.5em;
+}
+
+.pt-40 {
+  padding: 40px 0 50px 0;
+  display: flex;
+}
+</style>
