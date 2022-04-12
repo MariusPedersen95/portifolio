215cbb7567da399d997cb468ca846ba7c55d1491
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-04-12 13:08:58 +0200

    Create skills.vue  and added some dummy

---
+++ components/Skills.vue
@@ -0,0 +1,59 @@
+<template>
+  <section>
+    <div class="one-whole flex">
+      <div class="one-half">
+        <div class="one-third center">
+          <p class="">About Marius</p>
+        </div>
+      </div>
+      <div class="one-half">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit
+          dui volutpat donec adipiscing porttitor. Risus sed vulputate velit
+          convallis vel malesuada nisl lectus. Et integer nunc nulla vitae at
+          lectus nisl nulla quis.
+        </p>
+      </div>
+    </div>
+    <div class="one-whole flex mt3">
+      <div class="one-half">
+        <div class="one-third center">
+          <p class="">Skills</p>
+        </div>
+      </div>
+      <div class="one-half">
+        <p>HTML, vue, css osv. Craft, contentfuel.</p>
+      </div>
+    </div>
+    <div class="one-whole flex mt3">
+      <div class="one-half">
+        <div class="one-third center">
+          <p class="">About Marius</p>
+        </div>
+      </div>
+      <div class="one-half">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit
+          dui volutpat donec adipiscing porttitor. Risus sed vulputate velit
+          convallis vel malesuada nisl lectus. Et integer nunc nulla vitae at
+          lectus nisl nulla quis.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit
+          dui volutpat donec adipiscing porttitor. Risus sed vulputate velit
+          convallis vel malesuada nisl lectus. Et integer nunc nulla vitae at
+          lectus nisl nulla quis.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.mt3 {
+  margin-top: 3rem;
+}
+</style>
