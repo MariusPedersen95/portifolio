c019eedcbb2ff5553be3384129c6d765710bea59
Marius Pedersen <marius.pedersen95@hotmail.no>
2022-04-12 21:32:11 +0200

    Changed favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Frontend developer Marius Pedersen Oslo based.',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon-border.png',
+        href: '/favicon.png',
       },
       {
         rel: 'preconnect',
